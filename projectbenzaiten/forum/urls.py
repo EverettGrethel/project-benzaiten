fa01fd101aab72274e73dfbe762e5e7c75bb144a
RadicalEric <ericostfeld@gmail.com>
2020-07-29 19:48:59 -0400

    merged with updated code

--- projectbenzaiten/forum/urls.py
+++ projectbenzaiten/forum/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from .views import (
 CommunityView,
 CommunityListView,
+PostDetailView
 )
 
 urlpatterns = [
     path('', CommunityListView.as_view(), name='forum-index'),
-    path('forum/<int:id>', CommunityView.as_view(), name='forum-home')
-    
+    path('forum/<str:title>/<int:pk', PostDetailView.as_view(), name='post-detail'),
+    path('forum/<str:title>', CommunityView.as_view(), name='forum-home')
 ]
