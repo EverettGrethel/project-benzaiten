619955335c878cf6a2c6b8dd9d8b04e9230990a2
Everett Grethel <everett.grethel@uconn.edu>
2020-07-30 19:28:01 -0400

    Added user views and profile model

--- projectbenzaiten/projectbenzaiten/settings.py
+++ projectbenzaiten/projectbenzaiten/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'forum-index'
+LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
